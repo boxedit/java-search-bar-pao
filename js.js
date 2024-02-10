091d29c1c79d4c6b02e5086afcc53adc6dff1e09
boxedit <159624091+boxedit@users.noreply.github.com>
2024-02-10 19:13:00 +0700

    js.js

---
+++ js.js
@@ -0,0 +1,13 @@
+[
+   {
+      "title":"stardew valley (Thai)",
+      "link":"https://webdesign.tutsplus.com/tutorials/how-to-filter-data-on-a-webpage-with-javascript--cms-93330",
+      "description":"In this tutorial, we’ll look into using vanilla JavaScript to filter mock data (results on a blog listing page) according to their category.",
+      "image":"https://cms-assets.tutsplus.com/cdn-cgi/image/width=300/uploads/users/30/posts/93330/preview_image/filter_data_in_list.png",
+      "categories":[
+         "JavaScript",
+         "Filters"
+      ],
+      "level":"Intermediate"
+   },
+]
